427040f7d279a5ee1abde22f4123363e14d7733c
John G <johngouldman@pm.me>
2022-07-01 17:10:11 -0500

    completed

---
+++ main.js
@@ -0,0 +1,27 @@
+
+let para1 = document.querySelector('#para1');
+para1.style.color = "lightblue";
+
+let heading = document.querySelector('h1');
+heading.style.fontSize = '48px';
+
+para1.innerText = "Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress."
+
+
+let item13 = document.querySelector('#item-13');
+item13.style.opacity = .5;
+
+let item3 = document.querySelector('#item-3');
+item3.innerText = `I say, "Hi!"`
+
+let image1 = document.querySelector('img');
+image1.src = 'https://www.dictionary.com/e/wp-content/uploads/2019/02/1000x700-ninja-790x310.jpg';
+image1.style.height = '300px';
+
+let item16 = document.querySelector('#item-16');
+item16.className = 'item'
+item16.innerText = `Won't get fooled again.`;
+
+let image2 = document.querySelector('#image-2');
+image2.style.height = '300px';
+
